7e3cca533d68629ae5cd3c10810079424558ce7f
Rogue <rogue87@tuta.io>
2026-01-17 17:59:11 +0200

    sorta implement list command (needs work)

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,5 +1,17 @@
-use std::error::Error;
+use std::{error::Error, path::Path};
+
+use crate::config::Config;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    todo!()
+    let config_path = Path::new("dotsy.toml");
+    let config = Config::load_from_path(config_path).unwrap();
+
+    match config.profiles {
+        Some(profiles) => {
+            dbg!(profiles);
+        }
+        _ => println!("No profiles defined in config."),
+    }
+
+    return Ok(());
 }
